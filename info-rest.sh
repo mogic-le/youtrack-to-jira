aa55d330dc93b09fbbf8c3b9350cec06f014f8b0
Christian Weiske <weiske@mogic.com>
2016-03-24 11:01:02 +0100

    SMM-6: show priorities

--- info-rest.sh
+++ info-rest.sh
@@ -6,5 +6,8 @@ xmlstarlet sel -t -v '/issues/issue/field[@name="Type"]/valueId' -n restdata/iss
 echo Issue statuses:
 xmlstarlet sel -t -v '/issues/issue/field[@name="State"]/valueId' -n restdata/issues-*.xml |sort|uniq -c
 
+echo Issue priorities:
+xmlstarlet sel -t -v '/issues/issue/field[@name="Priority"]/valueId' -n restdata/issues-*.xml |sort|uniq -c
+
 echo Users:
 xmlstarlet sel -t -v '/issues/issue/field[@name="Assignee" or @name="updaterName" or @name="reporterName"]/value' -n restdata/issues-*.xml |sort|uniq -c
